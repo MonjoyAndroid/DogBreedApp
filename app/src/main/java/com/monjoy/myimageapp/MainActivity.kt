a3834aaba4728ffc41474e6b57423000fa9ca0e5
Monjoy <monjoy.mitra@microblocklabs.com>
2024-07-09 20:34:27 +0530

    structured code and increased visibility of code.

--- app/src/main/java/com/monjoy/myimageapp/MainActivity.kt
+++ app/src/main/java/com/monjoy/myimageapp/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val db = BreedDatabase.getDatabase(this)!!
